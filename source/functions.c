705febbbf728325c1fdfe18add134309616c8e59
Peely <philipmartinantolihao@gmail.com>
2025-03-08 23:41:07 +0800

    function file

---
+++ source/functions.c
@@ -0,0 +1,9 @@
+#include "defs.h"
+#include <stdio.h>
+
+void displayMainMenu(){
+  printf("[1] Battle!\n");
+  printf("[2] ComPetDium\n");
+  printf("[3] View Statistics\n");
+  printf("[0] Exit\n");
+};
